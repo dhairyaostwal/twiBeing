1a13f9c5f9403388a28ef98eb2fa097b311140f4
Dhairya <dhairya.ostwal@gmail.com>
2023-04-01 13:02:14 +0530

    Made Flask API ready!

--- app.py
+++ app.py
@@ -5,40 +5,63 @@ from flask import Flask
 from flask_restful import Resource, Api
 import os
 from dotenv import load_dotenv
-
 from util.TweetProcessing import (
     get_tweets,
     get_tweet_sentiment,
     count_frequency_tweet_sentiments,
 )
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class tweet_process_and_compute_result(Resource):
+    def get(self, username):
+        # to check for 404 error
+        try:
+            tweets_to_check_for = get_tweets(username)
+        except:
+            return {"error": "Username does not exist. Check again for possible typo."}
+
+        sentiments_collections = []
+        result = []
+
+        for tweet in tweets_to_check_for:
+            sentiments_collections.append(get_tweet_sentiment(tweet))
+
+        sentiments_result = count_frequency_tweet_sentiments(sentiments_collections)
+
+        positive_sentiment = 0
+        negative_sentiment = 0
+
+        for key, value in sentiments_result.items():
+            if key == "positive":
+                positive_sentiment = value
+            elif key == "negative":
+                negative_sentiment = value
+
+        statement = "final remark on username"
+
+        if negative_sentiment / positive_sentiment > 1:
+            neg_value_in_percentage = round(
+                (negative_sentiment / positive_sentiment) * 100
+            )
+            statement = (
+                username + " has negative tweets with " + neg_value_in_percentage + "%"
+            )
+        else:
+            statement = username + " is safe to interact with"
+
+        sentiments_result["remark"] = statement
+        result.append(sentiments_result)
+        return result
+
+
+api.add_resource(tweet_process_and_compute_result, "/predict/<username>")
+
 
 # Driver function
 if __name__ == "__main__":
-
-    # checking tweets for particular username
-    username = "jordanbpeterson"
-    
-    tweets_to_check_for = get_tweets(username)
-
-    sentiments_collections = []
-
-    for tweet in tweets_to_check_for:
-        sentiments_collections.append(get_tweet_sentiment(tweet))
-
-    result = count_frequency_tweet_sentiments(sentiments_collections)
-    print(result)
-
-    positive_sentiment = 0
-    negative_sentiment = 0
-
-    for key, value in result.items():
-        if key == "positive":
-            positive_sentiment = value
-        elif key == "negative":
-            negative_sentiment = value
-
-    if negative_sentiment / positive_sentiment > 1:
-        neg_value_in_percentage = round((negative_sentiment / positive_sentiment) * 100)
-        print("ALERT!\nNegativity of Tweets =", neg_value_in_percentage, "%")
-
-    print("CONCLUSION:\nAccount Username -", username, "is safe to interact with")
+    app.run(debug=True)
